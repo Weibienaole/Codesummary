c041e83b088b329cfef70240927c83de95bf9625
Weibienaole <weibienaole@163.com>
2021-03-24 21:30:54 +0800

    '3-24_win_loader'

--- webpack/P40-P46_loader/loader/css-loader.js
+++ webpack/P40-P46_loader/loader/css-loader.js
@@ -1,15 +1,28 @@
-function loader(source){
+function loader(source) {
+  // 校验全局有  url(内部包含特殊字符)
   let reg = /url\((.+?)\)/g
+  // 定位
   let pos = 0
-  let current = []
+  let current
+  // 设置初始值
   let arr = ['let list = []']
-  while(current = reg.exec(source)){
+  // while循环,只要有匹配到的值,就一直循环
+  while (current = reg.exec(source)) {
+    // 结构 matchUrl 是包含匹配项的内容 url('./pic.png')  g是匹配到的内容 './pic.png'
     let [matchUrl, g] = current
-    arr.push(`list.push(${JSON.stringify(source.slice(pos, reg.lastIndex - matchUrl.length))})`)
+    // 以push的形式将所有处理之后的内容包含在一个数组内,最后再以导出内部定义数组.join()的形式完成
+    // 拿到匹配值的前一位
+    let last = reg.lastIndex - matchUrl.length
+    // push进去 不修改的内容 (url()之前的内容)
+    arr.push(`list.push(${JSON.stringify(source.slice(pos, last))})`)
+    // 将url里面的内容改成require引入形式
     arr.push(`list.push('url(' + require(${g}) + ')')`)
+    // 完成一次循环将 pos 更新,进行下面的匹配
     pos = reg.lastIndex
   }
+  // push进去匹配之后的内容
   arr.push(`list.push(${JSON.stringify(source.slice(pos))})`)
+  // 以module.exports 的形式导出 内数组的join形式
   arr.push("module.exports = list.join('')")
   return arr.join('\r\n')
 }
--- webpack/P40-P46_loader/loader/style-loader.js
+++ webpack/P40-P46_loader/loader/style-loader.js
@@ -5,7 +5,12 @@ function loader(source) {
   document.head.appendChild(style)`
   return str
 }
+// 使用pitch之后，不再走向此loader(包括此loader)之后的loader
+ // remainingRequest 剩余的请求 剩余的loader 以绝对路径的形式展示
 loader.pitch = function(remainingRequest) {
+  // 利用loaderUtils.stringifyRequest拿到当前路径的工作路径然后以inline-loader的形式重新处理css 最后得到的结果是css-loader处理之后的内容，然后以 innerHTML 的形式插入到页面中
+
+  // reuqire路径 返回的就是css-loader处理好的结果  require('!!css-loader!less-loader!index.less')
   let str = `let style = document.createElement('style')
   style.innerHTML=require(${loaderUtils.stringifyRequest(this, '!!' + remainingRequest)})
   document.head.appendChild(style)`
